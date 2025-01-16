0b696f1ad1aa9ae3018c992f015d525c2254b11d
Howl <afhernandezb@gmail.com>
2025-01-15 22:31:23 -0300

    Busca Autores

--- src/main/java/com/aluracursos/literalura/principal/Principal.java
+++ src/main/java/com/aluracursos/literalura/principal/Principal.java
@@ -19,6 +19,7 @@ public class Principal {
     private List<Libro> libros;
     private IdiomaRepository repositorioDeIdiomas;
     private AutorRepository repositorioDeAutores;
+    private List<Autores> autoresExistentes;
 
     public Principal(LibroRepository libroRepository, IdiomaRepository idiomaRepository, AutorRepository autorRepository) {
         this.repositorioDeLibros = libroRepository;
@@ -50,6 +51,12 @@ public class Principal {
                 case 2:
                     listarLibrosBuscados();
                     break;
+                case 3:
+                    listarAutores();
+                    break;
+                case 4:
+                    autoresVivosPeriodo();
+                    break;
                 case 0:
                     System.out.println("Cerrando aplicacion...");
                 default:
@@ -68,43 +75,70 @@ public class Principal {
     private void getDatosLibro() {
         System.out.println("Escribe el nombre del libro que desea buscar");
         var nombreLibro = teclado.nextLine();
-        var json = consumoApi.obtenerDatos(URL_BASE + nombreLibro.replace(" ", "+").toLowerCase());
-        Resultado datos = conversor.obtenerDatos(json, Resultado.class);
-        DatosLibro consulta = datos.resultados().get(0);
-        Autores autoresConsulta =new Autores(consulta.autores().isEmpty()?
-                new DatosAutores("Desconocido",0, 0):consulta.autores().get(0));
-        Libro libroSave = new Libro(consulta);
-        Idioma idiomaSave = libroSave.getIdiomas();
-        repositorioDeIdiomas.save(idiomaSave);
-        List<Libro> primerLibro = repositorioDeLibros.findByTituloContainsIgnoreCase(libroSave.getTitulo());
-        List<Autores> primerAutor = repositorioDeAutores.findByNombreContainsIgnoreCase(autoresConsulta.getNombre());
-        List<Idioma> primerIdioma = repositorioDeIdiomas.findByAbreviacion(idiomaSave.getAbreviacion());
-
-        if(!primerIdioma.isEmpty()){
-            primerIdioma.forEach(i->{
-                if (idiomaSave.getAbreviacion().contains(i.getAbreviacion())){
-                    libroSave.setIdiomas(i);
-                }else {
-                    repositorioDeIdiomas.save(idiomaSave);
-                }
-            });
-        }else{
+        try {
+            var json = consumoApi.obtenerDatos(URL_BASE + nombreLibro.replace(" ", "+").toLowerCase());
+            Resultado datos = conversor.obtenerDatos(json, Resultado.class);
+            DatosLibro consulta = datos.resultados().get(0);
+            Autores autoresConsulta =new Autores(consulta.autores().isEmpty()?
+                    new DatosAutores("Desconocido",0, 0):consulta.autores().get(0));
+            Libro libroSave = new Libro(consulta);
+            Idioma idiomaSave = libroSave.getIdiomas();
             repositorioDeIdiomas.save(idiomaSave);
+            List<Libro> primerLibro = repositorioDeLibros.findByTituloContainsIgnoreCase(libroSave.getTitulo());
+            List<Autores> primerAutor = repositorioDeAutores.findByNombreContainsIgnoreCase(autoresConsulta.getNombre());
+            List<Idioma> primerIdioma = repositorioDeIdiomas.findByAbreviacion(idiomaSave.getAbreviacion());
+
+            if(!primerIdioma.isEmpty()){
+                primerIdioma.forEach(i->{
+                    if (idiomaSave.getAbreviacion().contains(i.getAbreviacion())){
+                        libroSave.setIdiomas(i);
+                    }else {
+                        repositorioDeIdiomas.save(idiomaSave);
+                    }
+                });
+            }else{
+                repositorioDeIdiomas.save(idiomaSave);
+            }
+
+            if (primerAutor.size() == 0 && primerLibro.size()==0){
+                repositorioDeAutores.save(autoresConsulta);
+                libroSave.setAutores(autoresConsulta);
+                repositorioDeLibros.save(libroSave);
+            } else if (primerAutor.size() != 0 && primerLibro.size() != 0) {
+                libroSave.setAutores(primerAutor.get(0));
+                repositorioDeLibros.save(libroSave);
+            }
+            System.out.println(libroSave);
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Libro no disponible");
+            muestraElMenu();
         }
 
-        if (primerAutor.size() == 0 && primerLibro.size()==0){
-            repositorioDeAutores.save(autoresConsulta);
-            libroSave.setAutores(autoresConsulta);
-            repositorioDeLibros.save(libroSave);
-        } else if (primerAutor.size() != 0 && primerLibro.size() != 0) {
-            libroSave.setAutores(primerAutor.get(0));
-            repositorioDeLibros.save(libroSave);
-        }
-        System.out.println("Titulo: " + libroSave.getTitulo()+'\n'+ "Autor: " + primerAutor +'\n'+"Idioma: " + primerIdioma +'\n' + "Descargas: "+ libroSave.getDescargas() );
     }
 
     private void listarLibrosBuscados(){
         libros = repositorioDeLibros.findAll();
         libros.forEach(System.out::println);
     }
+
+    private void listarAutores(){
+        autoresExistentes = repositorioDeAutores.findAll();
+        autoresExistentes.forEach(System.out::println);
+    }
+
+    private void autoresVivosPeriodo(){
+        System.out.println("Escriba el año que desea consultar autores vivos: ");
+        var anoAutor = teclado.nextInt();
+        List<Autores> autoresVivos = repositorioDeAutores.findByAno(anoAutor);
+        if (!autoresVivos.isEmpty()){
+            autoresVivos.forEach(System.out::println);
+        }else {
+            System.out.println("No se ecnotraron autores");
+        }
+    }
+
+    private void listarPorIdioma(){
+
+    }
+
 }
--- src/main/java/com/aluracursos/literalura/repository/AutorRepository.java
+++ src/main/java/com/aluracursos/literalura/repository/AutorRepository.java
@@ -2,11 +2,13 @@ package com.aluracursos.literalura.repository;
 
 import com.aluracursos.literalura.model.Autores;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autores, Integer> {
     List<Autores> findByNombreContainsIgnoreCase(String nombre);
-    List<Autores> findByAnoFallecimientoLessThanEqual(int anoMaximo);
-    List<Autores> findByAnoNacimientoBetween(int anoNacimiento, int hastaAno);
+
+    @Query("SELECT a FROM Autores a WHERE a.anoNacimiento <= :anoAutor AND a.anoFallecimiento >= :anoAutor ")
+    List<Autores> findByAno(int anoAutor);
 }
